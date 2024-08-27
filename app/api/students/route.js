bb3b68092f820398bc0e7b0fe65eab8b2c81683c
IsmailZhaf <ismailzhanfeari@gmail.com>
2024-08-27 13:56:48 +0700

    finish

--- app/api/students/route.js
+++ app/api/students/route.js
@@ -1,43 +1,43 @@
-import { db } from "@/utils";
-import { SANTRI } from "@/utils/schema";
-import { eq } from "drizzle-orm";
-import { NextResponse } from "next/server";
+import { db } from '@/utils';
+import { SANTRI } from '@/utils/schema';
+import { eq } from 'drizzle-orm';
+import { NextResponse } from 'next/server';
 
 export async function POST(req, res) {
-    const data = await req.json();
+  const data = await req.json();
 
-    const result = await db.insert(SANTRI).values({
-        nama: data?.nama,
-        kelas: data?.kelas,
-        asal: data?.asal,
-        NIS: data?.NIS,
-    });
+  const result = await db.insert(SANTRI).values({
+    nama: data?.nama,
+    kelas: data?.kelas,
+    angkatan: data?.angkatan,
+    NIS: data?.NIS,
+  });
 
-    return NextResponse.json(result);
+  return NextResponse.json(result);
 }
 
-export async function PUT(req, res){
-    const data = await req.json();
+export async function PUT(req, res) {
+  const data = await req.json();
 
-    const result = await db.insert(SANTRI).values({
-        nama: data?.nama,
-        kelas: data?.kelas,
-        asal: data?.asal,
-        NIS: data?.NIS,
-    });
+  const result = await db.insert(SANTRI).values({
+    nama: data?.nama,
+    kelas: data?.kelas,
+    angkatan: data?.angkatan,
+    NIS: data?.NIS,
+  });
 
-    return NextResponse.json(result);
+  return NextResponse.json(result);
 }
 
 export async function GET(req) {
-    const result = await db.select().from(SANTRI);
-    return NextResponse.json(result);
+  const result = await db.select().from(SANTRI);
+  return NextResponse.json(result);
 }
 
 export async function DELETE(req) {
-    const searchParams = req.nextUrl.searchParams;
-    const NIS = searchParams.get("NIS");
+  const searchParams = req.nextUrl.searchParams;
+  const NIS = searchParams.get('NIS');
 
-    const result = await db.delete(SANTRI).where(eq(SANTRI.NIS, NIS));
-    return NextResponse.json(result);
+  const result = await db.delete(SANTRI).where(eq(SANTRI.NIS, NIS));
+  return NextResponse.json(result);
 }
--- app/dashboard/santri/_componnents/AddNewStudent.jsx
+++ app/dashboard/santri/_componnents/AddNewStudent.jsx
@@ -1,107 +1,141 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { useForm } from "react-hook-form";
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
-import GlobalApi from "@/app/_services/GlobalApi";
-import { toast } from "sonner";
-import { LoaderIcon } from "lucide-react";
+'use client';
+import React, { useEffect, useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { useForm } from 'react-hook-form';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import GlobalApi from '@/app/_services/GlobalApi';
+import { toast } from 'sonner';
+import { LoaderIcon } from 'lucide-react';
 
-export default function AddNewStudent({refreshData}) {
-    const [open, setOpen] = useState(false);
-    const [grades, setGrades] = useState([]);
-    const [loading, setLoading] = useState(false);
+export default function AddNewStudent({ refreshData }) {
+  const [open, setOpen] = useState(false);
+  const [grades, setGrades] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    const {
-        register,
-        handleSubmit,
-        reset,
-        formState: { errors },
-    } = useForm();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
-    useEffect(() => {
-        GetAllGradesList();
-    }, []);
+  useEffect(() => {
+    GetAllGradesList();
+  }, []);
 
-    const GetAllGradesList = async () => {
-        try {
-            const resp = await GlobalApi.GetAllGrades();
-            setGrades(resp.data);
-        } catch (error) {
-            console.error("Failed to fetch grades", error);
-        }
-    };
+  const GetAllGradesList = async () => {
+    try {
+      const resp = await GlobalApi.GetAllGrades();
+      setGrades(resp.data);
+    } catch (error) {
+      console.error('Failed to fetch grades', error);
+    }
+  };
 
-    const onSubmit = async (data) => {
-        setLoading(true);
-        try {
-            const resp = await GlobalApi.CreateNewStudent(data);
-            console.log("---", resp);
-            if (resp.data) {
-                reset();
-                refreshData();
-                setLoading(false);
-                setOpen(false); // Close the dialog on successful submission
-                toast("New Student Added!");
-            }
+  const onSubmit = async (data) => {
+    console.log(data);
+    setLoading(true);
+    try {
+        const resp = await GlobalApi.CreateNewStudent(data);
+        console.log("---", resp);
+        if (resp.data) {
+            reset();
+            refreshData();
             setLoading(false);
-        } catch (error) {
-            console.error("Failed to create student", error);
+            setOpen(false); // Close the dialog on successful submission
+            toast("New Student Added!");
         }
-    };
+        setLoading(false);
+    } catch (error) {
+        console.error("Failed to create student", error);
+        setLoading(false);
+    }
+  };
 
-    return (
-        <div>
-            <Dialog open={open} onOpenChange={setOpen}>
-                <DialogTrigger asChild>
-                    <Button>+ Tambah Santri Baru</Button>
-                </DialogTrigger>
-                <DialogContent className="max-w-[350px]">
-                    <DialogHeader>
-                        <DialogTitle>Tambah Santri Baru</DialogTitle>
-                        <DialogDescription>
-                            <form onSubmit={handleSubmit(onSubmit)}>
-                                <div className="py-2">
-                                    <label>Nama Lengkap</label>
-                                    <Input placeholder="Ex. Ismail" {...register("nama", { required: true })} />
-                                    {errors.nama && <p className="text-red-500">Nama Lengkap is required</p>}
-                                </div>
-                                <div className="flex flex-col py-2">
-                                    <label>Pilih Kelas</label>
-                                    <select className="p-3 border rounded-lg" {...register("kelas", { required: true })}>
-                                        {grades.map((item, index) => (
-                                            <option key={index} value={item.kelas}>
-                                                {item.kelas}
-                                            </option>
-                                        ))}
-                                    </select>
-                                    {errors.kelas && <p className="text-red-500">Pilih Kelas is required</p>}
-                                </div>
-                                <div className="py-2">
-                                    <label>Angkatan</label>
-                                    <Input placeholder="Ex. 2021" {...register("angkatan", { required: true })} />
-                                    {errors.asal && <p className="text-red-500">Angkatan is required</p>}
-                                </div>
-                                <div className="py-2">
-                                    <label>NIS</label>
-                                    <Input type="number" placeholder="Ex. 998876423" {...register("NIS", { required: true })} />
-                                    {errors.NIS && <p className="text-red-500">NIS is required</p>}
-                                </div>
+  return (
+    <div>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button>+ Tambah Santri Baru</Button>
+        </DialogTrigger>
+        <DialogContent className="max-w-[350px]">
+          <DialogHeader>
+            <DialogTitle>Tambah Santri Baru</DialogTitle>
+            <DialogDescription>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="py-2">
+                  <label>Nama Lengkap</label>
+                  <Input
+                    placeholder="Ex. Ismail"
+                    {...register('nama', { required: true })}
+                  />
+                  {errors.nama && (
+                    <p className="text-red-500">Nama Lengkap is required</p>
+                  )}
+                </div>
+                <div className="flex flex-col py-2">
+                  <label>Pilih Kelas</label>
+                  <select
+                    className="p-3 border rounded-lg bg-white dark:bg-white"
+                    {...register('kelas', { required: true })}
+                  >
+                    {grades.map((item, index) => (
+                      <option key={index} value={item.kelas}>
+                        {item.kelas}
+                      </option>
+                    ))}
+                  </select>
+                  {errors.kelas && (
+                    <p className="text-red-500">Pilih Kelas is required</p>
+                  )}
+                </div>
+                <div className="py-2">
+                  <label>Angkatan</label>
+                  <Input
+                    placeholder="Ex. 2021"
+                    {...register('angkatan', { required: true })}
+                  />
+                  {errors.angkatan && (
+                    <p className="text-red-500">Angkatan is required</p>
+                  )}
+                </div>
+                <div className="py-2">
+                  <label>NIS</label>
+                  <Input
+                    type="number"
+                    placeholder="Ex. 998876423"
+                    {...register('NIS', { required: true })}
+                  />
+                  {errors.NIS && (
+                    <p className="text-red-500">NIS is required</p>
+                  )}
+                </div>
 
-                                <div className="flex gap-3 items-center justify-end mt-5">
-                                    <Button type="button" onClick={() => setOpen(false)} variant="ghost">
-                                        Cancel
-                                    </Button>
-                                    <Button type="submit" disable={loading}>
-                                        {loading ? <LoaderIcon className="animate-spin" /> : "Save"}
-                                    </Button>
-                                </div>
-                            </form>
-                        </DialogDescription>
-                    </DialogHeader>
-                </DialogContent>
-            </Dialog>
-        </div>
-    );
+                <div className="flex gap-3 items-center justify-end mt-5">
+                  <Button
+                    type="button"
+                    onClick={() => setOpen(false)}
+                    variant="ghost"
+                  >
+                    Cancel
+                  </Button>
+                  <Button type="submit" disabled={loading}>
+                    {loading ? <LoaderIcon className="animate-spin" /> : 'Save'}
+                  </Button>
+                </div>
+              </form>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 }
